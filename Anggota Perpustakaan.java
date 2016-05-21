6c3ab7b0ed564e8c4297b69b5f57732f26ade20c
AyuAzizah <60200113036@uin-alauddin.ac.id>
2016-05-21 21:56:28 +0700

    versi 1.0.0

---
+++ Anggota Perpustakaan.java
@@ -0,0 +1,19 @@
+package Petugas Perpustakaan;
+
+import java.util.Vector;
+
+public class Anggota Perpustakaan {
+
+  public int IDAnggota;
+
+  public String Nama;
+
+  public String Jurusan;
+
+    public Vector  Registrasi;
+
+  public Void AddAnggota() {
+  return null;
+  }
+
+}
---
+++ Petugas Perpustakaan.java
@@ -0,0 +1,35 @@
+import java.util.Vector;
+import Petugas Perpustakaan.Void;
+import Petugas Perpustakaan.String;
+import Petugas Perpustakaan.int;
+
+public class Petugas Perpustakaan {
+
+  public int IDPetugas;
+
+  public String Nama;
+
+    public Vector  Registrasi;
+public class Anggota Perpustakaan {
+
+  public int IDAnggota;
+
+  public String Nama;
+
+  public String Jurusan;
+
+    public Vector  Registrasi;
+
+  public Void AddAnggota() {
+  return null;
+  }
+
+}public class int {
+}public class String {
+}public class Void {
+}
+  public Void AddPetugas() {
+  return null;
+  }
+
+}
